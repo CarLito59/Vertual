2ab7a6d1431d79431ee17855f88258060efc7be4
Vertual <36052239+CarLito59@users.noreply.github.com>
2018-03-04 20:44:24 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const util = require('util');
 const client = new Discord.Client();
-
+client.on("guildMemberAdd", member => {
+/*Ton code*/
+})
 
 client.login(process.env.TOKEN);
